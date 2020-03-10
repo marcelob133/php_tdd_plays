e23aca2a7a98ad7fbf2173345a34ee39c98010bf
marcelob133 <marceloborellihegedus@gmail.com>
2020-03-09 22:18:18 -0300

    first commit

---
+++ code/tests/TesteTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace tests;
+
+use PHPUnit\Framework\TestCase;
+
+class TesteTest extends TestCase
+{
+  public function test_variable_exists(){
+    $nome = "Alexandre";
+    $this->assertEquals("Alexandre", $nome);
+  }
+}
